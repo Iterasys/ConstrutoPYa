5576bc224d1e270a98b73a1adf01bd9d645a4e5f
Iterasys <40407006+Iterasys@users.noreply.github.com>
2021-10-06 22:33:16 -0300

    Criação do teste de Post e Get na entidade User da PetStore

--- testes_api_petstore.py
+++ testes_api_petstore.py
@@ -1,8 +1,56 @@
 import pytest
 import requests
-import json
+
+url = 'https://petstore.swagger.io/v2/user'
 
 def testar_incluir_usuario():
+    # Configura
+    status_code_esperado = 200      # comunicação
+    codigo_esperado = 200           # funcional
+    tipo_esperado = 'unknown'       # fixo como desconhecido
+    mensagem_esperada = '12181'     # id do usuário
 
+    headers = {'Content-Type': 'application/json'}
 
-    resposta = requests.post('https://petstore.swagger.io/v2/user')
+    # Executa
+    resposta = requests.post(url=url,
+                             data=open('json/usuario1.json', 'rb'),
+                             headers=headers
+                             )
+    corpo_da_resposta = resposta.json()
+    print(resposta) # Status Code
+    print(resposta.status_code) # Status Code
+    print(corpo_da_resposta) # Response Body / Corpo da Resposta
+
+    # Valida
+    assert resposta.status_code == status_code_esperado
+    assert corpo_da_resposta['code'] == codigo_esperado
+    assert corpo_da_resposta['type'] == tipo_esperado
+    assert corpo_da_resposta['message'] == mensagem_esperada
+
+def testar_consultar_usuario():
+    # Configura
+    username = 'correia' # input / entrada para a consulta
+    id_esperado = 12181
+    username_esperado = 'correia'
+    email_esperado = 'test@iterasys.com.br'
+    telefone_esperado = '11999992181'
+    user_status_esperado = 0
+    status_code_esperado = 200      # comunicação
+
+    headers = {'Content-Type': 'application/json'}
+
+    # Executa
+    resposta = requests.get(f'{url}/{username}', headers=headers)
+
+    corpo_da_resposta = resposta.json()
+    print(resposta) # Status Code
+    print(resposta.status_code) # Status Code
+    print(corpo_da_resposta) # Response Body / Corpo da Resposta
+
+    assert resposta.status_code == status_code_esperado
+    assert corpo_da_resposta['id'] == id_esperado
+    assert corpo_da_resposta['username'] == username_esperado
+    assert corpo_da_resposta['email'] == email_esperado
+    assert corpo_da_resposta['phone'] == telefone_esperado
+    assert corpo_da_resposta['userStatus'] == user_status_esperado
