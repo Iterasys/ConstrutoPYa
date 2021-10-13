4b00006c49a1a7a0dd397bcb2593077a3239078e
Iterasys <40407006+Iterasys@users.noreply.github.com>
2021-10-13 20:35:18 -0300

    Criação dos métodos de teste Put e Delete na entidade User da PetStore

--- testes_api_petstore.py
+++ testes_api_petstore.py
@@ -3,12 +3,13 @@ import requests
 
 url = 'https://petstore.swagger.io/v2/user'
 
+
 def testar_incluir_usuario():
     # Configura
-    status_code_esperado = 200      # comunicação
-    codigo_esperado = 200           # funcional
-    tipo_esperado = 'unknown'       # fixo como desconhecido
-    mensagem_esperada = '12181'     # id do usuário
+    status_code_esperado = 200  # comunicação
+    codigo_esperado = 200  # funcional
+    tipo_esperado = 'unknown'  # fixo como desconhecido
+    mensagem_esperada = '12181'  # id do usuário
 
     headers = {'Content-Type': 'application/json'}
 
@@ -18,9 +19,9 @@ def testar_incluir_usuario():
                              headers=headers
                              )
     corpo_da_resposta = resposta.json()
-    print(resposta) # Status Code
-    print(resposta.status_code) # Status Code
-    print(corpo_da_resposta) # Response Body / Corpo da Resposta
+    print(resposta)  # Status Code
+    print(resposta.status_code)  # Status Code
+    print(corpo_da_resposta)  # Response Body / Corpo da Resposta
 
     # Valida
     assert resposta.status_code == status_code_esperado
@@ -28,15 +29,16 @@ def testar_incluir_usuario():
     assert corpo_da_resposta['type'] == tipo_esperado
     assert corpo_da_resposta['message'] == mensagem_esperada
 
+
 def testar_consultar_usuario():
     # Configura
-    username = 'correia' # input / entrada para a consulta
+    username = 'correia'  # input / entrada para a consulta
     id_esperado = 12181
     username_esperado = 'correia'
     email_esperado = 'test@iterasys.com.br'
     telefone_esperado = '11999992181'
     user_status_esperado = 0
-    status_code_esperado = 200      # comunicação
+    status_code_esperado = 200  # comunicação
 
     headers = {'Content-Type': 'application/json'}
 
@@ -44,13 +46,66 @@ def testar_consultar_usuario():
     resposta = requests.get(f'{url}/{username}', headers=headers)
 
     corpo_da_resposta = resposta.json()
-    print(resposta) # Status Code
-    print(resposta.status_code) # Status Code
-    print(corpo_da_resposta) # Response Body / Corpo da Resposta
+    print(resposta)  # Status Code
+    print(resposta.status_code)  # Status Code
+    print(corpo_da_resposta)  # Response Body / Corpo da Resposta
 
+    # Valida
     assert resposta.status_code == status_code_esperado
     assert corpo_da_resposta['id'] == id_esperado
     assert corpo_da_resposta['username'] == username_esperado
     assert corpo_da_resposta['email'] == email_esperado
     assert corpo_da_resposta['phone'] == telefone_esperado
     assert corpo_da_resposta['userStatus'] == user_status_esperado
+
+
+def testar_alterar_usuario():
+    # Configura
+    user = 'correia'
+    status_code_esperado = 200  # comunicação
+    codigo_esperado = 200  # funcional
+    tipo_esperado = 'unknown'  # fixo como desconhecido
+    mensagem_esperada = '12181'  # id do usuário
+
+    headers = {'Content-Type': 'application/json'}
+
+    # Executa
+
+    resposta = requests.put(url=f'{url}/{user}',
+                            data=open('json/usuario2.json', 'rb'),
+                            headers=headers
+                            )
+    corpo_da_resposta = resposta.json()
+    print(resposta)  # Status Code
+    print(resposta.status_code)  # Status Code
+    print(corpo_da_resposta)  # Response Body / Corpo da Resposta
+
+    # Valida
+    assert resposta.status_code == status_code_esperado
+    assert corpo_da_resposta['code'] == codigo_esperado
+    assert corpo_da_resposta['type'] == tipo_esperado
+    assert corpo_da_resposta['message'] == mensagem_esperada
+
+
+def testar_excluir_usuario():
+    # Configura
+    username = 'correia'  # input / entrada para a exclusão e retorno na resposta
+    tipo_esperado = 'unknown'
+    status_code_esperado = 200  # comunicação
+
+    headers = {'Content-Type': 'application/json'}
+
+    # Executa
+    resposta = requests.delete(f'{url}/{username}', headers=headers)
+
+    corpo_da_resposta = resposta.json()
+    print(resposta)  # Status Code
+    print(resposta.status_code)  # Status Code
+    print(corpo_da_resposta)  # Response Body / Corpo da Resposta
+
+    # Valida
+    assert resposta.status_code == status_code_esperado
+    assert corpo_da_resposta['code'] == status_code_esperado
+    assert corpo_da_resposta['type'] == tipo_esperado
+    assert corpo_da_resposta['message'] == username
+Criação do teste de Post e Get
